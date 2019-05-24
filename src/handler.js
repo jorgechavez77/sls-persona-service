1d289e233b921e7ae65a52617a7af9e32a01e726
Rodrigo Narvaez <rodadev47@gmail.com>
2019-05-24 16:37:19 -0400

    feat: Find Persona 1. Added locally prettier and ignored in .gitignore 2. removed yarn from start script in package json 3. Added a custom environment in serverless to avoid creating .env file 4. Added find persona handler

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,6 @@
 const repo = require('./repository')
 
-exports.createPersona = async (event) => {
+exports.createPersona = async event => {
   const data = JSON.parse(event.body)
   try {
     const id = await repo.createPersona(data)
@@ -8,15 +8,35 @@ exports.createPersona = async (event) => {
     const resp = await repo.findPersona(id)
     return {
       statusCode: 201,
-      body: JSON.stringify(resp)
+      body: JSON.stringify(resp),
     }
   } catch (error) {
     console.error(error)
     return {
       statusCode: 500,
       body: JSON.stringify({
-        message: error.message
-      })
+        message: error.message,
+      }),
+    }
+  }
+}
+
+module.exports.findPersona = async event => {
+  const { id } = event.pathParameters
+
+  try {
+    const resp = await repo.findPersona(id)
+    return {
+      statusCode: 201,
+      body: JSON.stringify(resp),
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: error.message,
+      }),
     }
   }
 }
